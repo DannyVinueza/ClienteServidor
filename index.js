23c7841442b5261d643ddee4aa9be1d46bc7ad84
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-06-04 12:07:52 -0500

    Envado de json

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const http = require("http");
 const { CLIENT_RENEG_LIMIT } = require("tls");
 //Enviando etiquetas html
+// const server = http.createServer((req, res)=>{
+//     res.setHeader('Content-type', 'text/html');
+//     res.write("<h1 style='color: yellow'>Hello Danny</h1>");
+//     res.end();
+// })
+
 const server = http.createServer((req, res)=>{
-    res.setHeader('Content-type', 'text/html');
-    res.write("<h1 style='color: yellow'>Hello Danny</h1>");
+    usuario = {
+        user: "Danny Vinueza",
+        email: "danny.vinueza@epn.edu.ec"
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.write(JSON.stringify(usuario));
     res.end();
 })
 
